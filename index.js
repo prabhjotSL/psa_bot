cd8e24d53b121fcd81477fd24a45659fd27eb868
Prabhjot Singh Lamba <prabhjotsingh.l@imidomain.com>
2017-01-08 15:40:06 +0530

    Added imichat integration

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 const request = require('request')
 const app = express()
 
-let rooms = {}
+let rooms = {} // {"facebook_id": {room_id: room_id, isBotEnabled: true}}
 
 app.set('port', (process.env.PORT || 5000))
 
@@ -21,6 +21,23 @@ app.get('/', function (req, res) {
 	res.send('hello world i am a secret bot')
 })
 
+app.post('/imichatmt', function(req, res) {
+	// request for message from Agent comes here.
+	console.log(req.body)
+	console.log(req.headers)
+	var message = req.body.channels["OTT-Messaging"].fb.text
+	var psid = req.body.destination[0].psid[0]
+	sendTextMessage(psid, message) // Change this PSID to PSID of the demo messenger client.
+})
+
+app.post('/imichatclosed', function(req, res) {
+	console.log(req.headers)
+	// this means that the chat has been closed in imichat and we need to reset isBotEnabled to true.
+	var chatID = req.headers["Chatid"]
+	console.log(chatID)
+	rooms["1069317166466636"].isBotEnabled = true
+})
+
 // for facebook verification
 app.get('/webhook/', function (req, res) {
 	if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
@@ -31,6 +48,7 @@ app.get('/webhook/', function (req, res) {
 
 // to post data
 app.post('/webhook/', function (req, res) {
+	console.log(req);
 	let messaging_events = req.body.entry[0].messaging
 	for (let i = 0; i < messaging_events.length; i++) {
 		let event = req.body.entry[0].messaging[i]
@@ -45,20 +63,140 @@ app.post('/webhook/', function (req, res) {
 			// sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
 
 			console.log(rooms)
-			sendSenderAction(sender, "mark_seen")
-			sendSenderAction(sender, "typing_on")
-			sendAPICall(event.message.text, sender)
+			if(rooms[sender] && !rooms[sender].isBotEnabled) {
+				sendSingleMessageToImichat(sender, event.message.text, rooms[sender].room_id)
+			} else {
+				sendSenderAction(sender, "mark_seen")
+				sendSenderAction(sender, "typing_on")
+				sendAPICall(event.message.text, sender)
+			}
 
 		}
 		else if (event.postback) {
 			let text = JSON.stringify(event.postback)
-			sendSenderAction(sender, "mark_seen")
-			sendSenderAction(sender, "typing_on")
-			sendAPICall(text, sender)
+			if(rooms[sender] && !rooms[sender].isBotEnabled) {
+				sendSingleMessageToImichat(sender, text)
+			} else {
+				sendSenderAction(sender, "mark_seen")
+				sendSenderAction(sender, "typing_on")
+				sendAPICall(text, sender)
+			}
 		}
 	}
 	res.sendStatus(200)
 })
+
+function sendSingleMessageToImichat(sender, message, roomId) {
+	var dateMsg = new Date()
+	var reqBody = {
+	    "submittedOn": dateMsg.getTime(),
+	    "appId": "a_20170106",
+	    "ts": dateMsg.toISOString(),
+	    "locale": "en_GB",
+	    "psid": "1069317166466636",
+	    "masterProfile": "Sandbox_cpms_1",
+	    "message": message,
+	    "timestamp": dateMsg.getTime().toString(),
+	    "timezone": 5.5,
+	    "tenant": "1",
+	    "event": "MO",
+	    "name": "Imimobile Hyd",
+	    "gender": "male",
+	    "fetchedOn": dateMsg.getTime(),
+	    "tid": roomId + "_" + randomInt(0,10000000),
+	    "profile_pic": "https://scontent.xx.fbcdn.net/v/t1.0-1/1918189_124190034639475_6618885323877539072_n.jpg?oh=43229a43531ce83576125f6e53cbfb23&oe=5917A3E8",
+	    "channel": "fb"
+	}
+	var reqParams = {
+		teamid: 12,
+		servicekey: 'B2160D34-E8D5-4C7A-A7A4-E2367F1ED2E2'
+	}
+	request({
+		url: 'https://notify.imichat.io/ngmp2chat/imiconnectFB.aspx',
+		params: reqParams,
+		headers: {
+			'content-type': 'application/json'
+		},
+		json: reqBody
+	}, function(error, response, body) {
+		if (error) {
+			console.log('Error sending single message to IMIChat: ', error)
+		} else if (response.body.error) {
+			console.log('Error in IMIChat: ', response.body.error)
+		} else if (!error && response.statusCode == 200) {
+			console.log("Sent Single Message to IMIChat")
+		}
+	})
+}
+
+function formatMessagesForIMIChat(messages) {
+	console.log("Messages: ", messages)
+	var new_messages = []
+    for(var i=0; i<messages.length; i++) {
+      var date_iso_format = (new Date(messages[i].created_at)).toISOString()
+      var single_message = {
+        date: date_iso_format,
+        text: messages[i].message
+      }
+
+      if(messages[i].userType == "bot") {
+        single_message.type = "MT"
+        new_messages.push(single_message)
+      } else if(messages[i].type == "human") {
+        single_message.type = "MO"
+        new_messages.push(single_message)
+
+        date_agent_iso_format = (new Date(messages[i].created_at + 2000)).toISOString()
+        var single_message = {
+          date: date_agent_iso_format,
+          text: messages[i].message,
+          type: "MT"
+        }
+
+        new_messages.push(single_message)
+      }
+
+    }
+
+    return new_messages
+}
+
+function randomInt(low, high) {
+    return Math.floor(Math.random() * (high - low) + low);
+}
+
+function sendMessagesToImichat(sender, body, messages) {
+	var messages = formatMessagesForIMIChat(messages)
+	var reqBody = {
+	    "customerid": "",
+	    "account" : "prabhjot",
+	    "chatid": body.room._id + "_" + randomInt(0, 10000000),
+	    "channel": "facebook",
+	    "fbpageid":"1656282471307602",
+	    "message": messages
+	}
+
+	request({
+		url: 'https://demo.imichat.io/chatapi/createchat',
+		headers: {
+			'X-IMI-AUTHKEY': '67183aaf-b7e8-422e-afc3-eb4cacc214fe',
+			'content-type': 'application/json'
+		},
+		method: 'POST',
+		json: reqBody
+	}, function(error, request, body) {
+		if (error) {
+			console.log('Error sending messages to IMIChat: ', error)
+			sendTextMessage(sender, body.generated_msg)
+		} else if (response.body.error) {
+			console.log('Error in IMIChat: ', response.body.error)
+			sendTextMessage(sender, body.generated_msg)
+		} else if (!error && response.statusCode == 200) {
+			rooms[body.consumer.facebookId].isBotEnabled = false
+			sendTextMessage(sender, body.generated_msg)
+		}
+	})
+}
 
 
 // recommended to inject access tokens as environmental variables, e.g.
@@ -82,7 +220,7 @@ function sendAPICall(text, sender) {
 			msg: text,
 			type: "human",
 			platform: "facebook",
-			room_id: rooms[sender] ? rooms[sender] : null
+			room_id: rooms[sender] ? rooms[sender].room_id : null
 		}
 	}, function(error, response, body) {
 		sendSenderAction(sender, "typing_off")
@@ -94,9 +232,17 @@ function sendAPICall(text, sender) {
 			console.log(body) // Show the HTML for the Google homepage.
 
 			if(!rooms[body.consumer.facebookId]) {
-				rooms[body.consumer.facebookId] = body.room._id // This has to be stored and retrieved from Mongo and not in Memory.
+				rooms[body.consumer.facebookId].room_id = body.room._id // This has to be stored and retrieved from Mongo and not in Memory.
+				rooms[body.consumer.facebookId].isBotEnabled = true
 			}
-			if(body.generated_msg) {
+			if(body.sendtoagent) {
+				// Here we will send all the messages of the room (max 15), to imichat, and on confirmation of that send a message to user saying connected to an agent.
+				// If the imichat api call fails, then send a message saying we will get back to you soon, our customer executives are busy.
+				// Send is isBotEnabled to false. So next time the message can go to imichat.
+				sendMessagesToImichat(sender, body, body.messages)
+				// rooms[body.consumer.facebookId].isBotEnabled = false
+			}
+			else if(body.generated_msg) {
 				if(body.generated_msg.type) {
 					if(body.generated_msg.type == "facebook_button") {
 						sendDynamicMessage(sender, body.generated_msg.buttons)
