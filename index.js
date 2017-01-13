cd97b113b54dfd0d9396a4603df75373f62e8f43
Prabhjot Singh Lamba <prabhjotsingh.l@imidomain.com>
2017-01-13 08:16:15 +0530

    Added code for prabhjot imichat

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const bodyParser = require('body-parser')
 const request = require('request')
 const app = express()
 
+const ankurid = "1496704427010794"
+const prabhjotid = "1069317166466636"
+
 let rooms = {} // {"facebook_id": {room_id: room_id, isBotEnabled: true}}
 
 app.set('port', (process.env.PORT || 5000))
@@ -27,7 +30,7 @@ app.post('/imichatmt', function(req, res) {
 	// console.log(req.headers)
 	var message = req.body.channels["OTT-Messaging"].fb.text
 	var psid = req.body.destination[0].psid[0]
-	sendTextMessage("1496704427010794", message) // Change this PSID to PSID of the demo messenger client.
+	sendTextMessage(prabhjotid, message) // Change this PSID to PSID of the demo messenger client.
 	setTimeout(function() {
 		res.json({
 			"response":[{
@@ -45,7 +48,7 @@ app.post('/imichatclosed', function(req, res) {
 	// this means that the chat has been closed in imichat and we need to reset isBotEnabled to true.
 	// var chatID = req.headers["Chatid"]
 	// console.log(chatID)
-	rooms["1496704427010794"].isBotEnabled = true // TODO: Change this ID to ID of ALEX.
+	rooms[prabhjotid].isBotEnabled = true // TODO: Change this ID to ID of ALEX.
 	setTimeout(function() {
 		res.json({success: true})
 	}, 1000)
