4eeec6d1852c43d3ad1ab84c0dc86f13e7eb9b9e
Prabhjot Singh Lamba <prabhjotsingh.l@imidomain.com>
2017-01-13 12:22:41 +0530

    added code so that  imichat only works end to end for ankur or prabhjot whichever is active

--- index.js
+++ index.js
@@ -352,7 +352,7 @@ function sendAPICall(text, sender) {
 						rooms[body.consumer.facebookId].room_id = body.room._id // This has to be stored and retrieved from Mongo and not in Memory.
 						rooms[body.consumer.facebookId].isBotEnabled = true
 					}
-					if(body.sendtoagent && enableIMIChatIntegration) {
+					if(body.sendtoagent && sender == ankurid && enableIMIChatIntegration) {
 						// Here we will send all the messages of the room (max 15), to imichat, and on confirmation of that send a message to user saying connected to an agent.
 						// If the imichat api call fails, then send a message saying we will get back to you soon, our customer executives are busy.
 						// Send is isBotEnabled to false. So next time the message can go to imichat.
